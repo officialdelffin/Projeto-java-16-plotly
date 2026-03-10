b1f13ad88439939760de97e5d127ec2f37be9d31
officialdelffin <workdelffin@gmail.com>
2026-03-10 07:47:19 -0300

    commit 15

--- src/br/com/plotly/view/Menu.java
+++ src/br/com/plotly/view/Menu.java
@@ -3,17 +3,25 @@
 package br.com.plotly.view;
 
 
+import java.util.Scanner;
+
 // Classe responsável pela comunicação direta com o usuário :
 public class Menu {
 
 
+    // Atributos :
     private String responseUser;
+
+
+    // Instancias :
+    Scanner inputUser = new Scanner(System.in);
 
 
     // Realiza o fluxo de interação do usuário :
     public void interactionUser() {
 
 
+        // Apresentação e armazenando a escolhar do movie :
         System.out.printf("""
                 
                 Olá! Seja bem-vindo ao Plotly. O seu catálogo digital está pronto.
@@ -21,6 +29,9 @@ public class Menu {
                 Qual título você deseja explorar hoje?
                 
                 """);
+
+        // Armazenando a escolher do user :
+        responseUser = inputUser.nextLine();
 
 
     }
