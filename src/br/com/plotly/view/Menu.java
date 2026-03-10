3075520cf884a584028cd8bc5678efe86fda36e0
officialdelffin <workdelffin@gmail.com>
2026-03-10 07:21:45 -0300

    commit 12

--- src/br/com/plotly/view/Menu.java
+++ src/br/com/plotly/view/Menu.java
@@ -3,11 +3,24 @@
 package br.com.plotly.view;
 
 
-
+// Classe responsável pela comunicação direta com o usuário :
 public class Menu {
 
 
+    // Realiza o fluxo de interação do usuário :
+    public void interactionUser() {
 
+
+        System.out.printf("""
+                
+                Olá, seja bem-vindo ao Plotly!
+                
+                Seu sistema de streaming, onde é possível assistir qualquer filme a qualquer momento.
+                
+                """);
+
+
+    }
 
 
 }
